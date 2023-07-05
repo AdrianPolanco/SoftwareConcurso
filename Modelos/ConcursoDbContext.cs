97ce576810d47c45742022cf6f9e03e49836a790
Adrián Saul Polanco Ferrer <Adrian>
2023-07-04 23:30:15 -0400

    TailwindCSS instalado correctamente

--- Modelos/ConcursoDbContext.cs
+++ Modelos/ConcursoDbContext.cs
@@ -21,7 +21,7 @@ public partial class ConcursoDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-9AJGK0OL;Initial Catalog=CONCURSO_DB;Persist Security Info=True;TrustServerCertificate=true;User ID=sa;Password=0611;");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-SU18V4P\\SQLEXPRESS;Initial Catalog=CONCURSO_DB;Persist Security Info=True;TrustServerCertificate=true;User ID=sa;Password=06112001;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
